4dc5b9bf4b8ecae79a48f7182d7727d13ac2c49d
ALexander Davis <ajdavis2424@gmail.com>
2022-02-08 12:53:28 -0500

    initial commit

---
+++ installs.md
@@ -0,0 +1,10 @@
+<!-- guild out of components -->
+ng g c welcome
+ng g c question
+ng g c header
+<!-- run app -->
+ng serve
+
+<!-- Bootstrap -->
+
+<!-- FontAewsoem -->
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuestionComponent } from './question/question.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+// PATH FOR COMPONENT ROUTES
+const routes: Routes = [
+  {path:"", redirectTo: 'welcome', pathMatch:"full"},
+  {path:"welcome", component:WelcomeComponent},
+  {path:"question", component:QuestionComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
