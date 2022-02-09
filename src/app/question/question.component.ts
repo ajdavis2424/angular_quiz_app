b2640bb76306baa810673e684af06311e436f423
ALexander Davis <ajdavis2424@gmail.com>
2022-02-08 22:15:56 -0500

    more logic, finishing counter portion

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -9,13 +9,59 @@ import { QuestionService } from '../service/question.service';
 export class QuestionComponent implements OnInit {
 
   public name : string="";
+  public questionList : any =[];
+  public currentQuestion: number = 0;
+  public points : number=0;
+  counter = 60;
+  correctAnswer:number=0;
+  incorrectAnswer:number=0;
+  interval$:any;
+
   constructor(private questionService : QuestionService) { }
 
   // Initialize
   ngOnInit(): void {
     this.name = localStorage.getItem("name")!;
+    this.getAllQuestions();
   }
   getAllQuestions(){
+this.questionService.getQuestionJson()
+.subscribe(res=>{
+  // Store questions in question list
+  this.questionList = res.questions;
+})
+  }
+// Methods for page navigation (currentQuestion array)!
+  nextQuestion(){
+    this.currentQuestion++;
+  }
+
+  previousQuestion(){
+    this.currentQuestion--
+  }
+
+// ANSWER METHOD
+answer(currentQno:number,option:any){
+  if(option.correct){
+    this.points+=10;
+    this.correctAnswer++;
+    this.currentQuestion++
+  } else{
+    this.points-=10;
+    this.currentQuestion++;
+    this.incorrectAnswer++;
+  }
+}
+
+// METHOD FOR COUNTER
+  startCounter(){
 
   }
+  stopCounter(){
+
+  }
+  resetCounter(){
+
+  }
+
 }
