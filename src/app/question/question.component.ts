5ec03f571d7c9771a2c74553b1cef1da9379722e
ALexander Davis <ajdavis2424@gmail.com>
2022-02-08 22:49:45 -0500

    fixed arrow buttons, logic 90 percent done

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionService } from '../service/question.service';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-question',
@@ -16,6 +17,7 @@ export class QuestionComponent implements OnInit {
   correctAnswer:number=0;
   incorrectAnswer:number=0;
   interval$:any;
+  progress:string="0";
 
   constructor(private questionService : QuestionService) { }
 
@@ -23,6 +25,7 @@ export class QuestionComponent implements OnInit {
   ngOnInit(): void {
     this.name = localStorage.getItem("name")!;
     this.getAllQuestions();
+    this.startCounter();
   }
   getAllQuestions(){
 this.questionService.getQuestionJson()
@@ -37,7 +40,7 @@ this.questionService.getQuestionJson()
   }
 
   previousQuestion(){
-    this.currentQuestion--
+    this.currentQuestion--;
   }
 
 // ANSWER METHOD
@@ -53,15 +56,42 @@ answer(currentQno:number,option:any){
   }
 }
 
-// METHOD FOR COUNTER
+// METHOD FOR COUNTER ---interval is imported from RX.js.
   startCounter(){
-
+    this.interval$ = interval(1000)
+    .subscribe(val=>{
+      this.counter--;
+      if(this.counter===0){
+        this.currentQuestion++
+        this.counter=60;
+        this.points-=10;
+      }
+    });
+    setTimeout(() => {
+      this.interval$.unsubscribe()
+    }, 6000000);
   }
   stopCounter(){
-
+    this.interval$.unsubscribe();
+    this.counter=0;
   }
   resetCounter(){
-
+    this.stopCounter();
+    this.counter=60;
+    this.startCounter();
   }
 
+// METHOD FOR RESETTING THE QUIZ
+  resetQuiz(){
+    this.resetCounter();
+    this.getAllQuestions();
+    this.points=0;
+    this.counter=60;
+    this.currentQuestion=0;
+  }
+
+  // PROGRESS BAR PERCENT METHOD
+  getProgressPercent(){
+    this.progress = ((this.currentQuestion/this.questionList.length)*100).toString()
+  }
 }
